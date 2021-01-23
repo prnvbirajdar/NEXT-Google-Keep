efe951fc89e4c507328bc14864e8b0b7fc17c5d8
prnvbirajdar <prnvbirajdar@gmail.com>
2021-01-23 05:03:27 -0500

    icons fixed

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,6 @@ import KeepIcon from "./IconComponents/KeepIcon";
 import Moon from "./IconComponents/Moon";
 import Sun from "./IconComponents/Sun";
 
-
 const Navbar = () => {
   const { dark, toggleDark, setDark } = useContext(ThemeContext);
 
@@ -16,20 +15,26 @@ const Navbar = () => {
   };
 
   return (
-    <div className=" transition duration-300 w-screen flex flex-row items-center p-1.5 md:px-4 justify-between bg-white shadow dark:bg-background border-b dark:border-gray-600 z-20 fixed top-0">
+    <div className=" transition duration-300 w-screen flex flex-row items-center p-1.5 md:px-4 pr-2 justify-between bg-white shadow dark:bg-background border-b dark:border-gray-600 z-20 fixed top-0">
       <div className="flex">
         <KeepIcon />
-        <p className=" text-xl  self-center font-medium text-gray-700 ml-1  dark:text-offwhite">
+        <p className=" text-xl  self-center font-medium text-gray-700 ml-1 dark:text-offwhite">
           Keep
         </p>
       </div>
 
       {dark ? (
-        <div onClick={handleClick}>
+        <div
+          onClick={handleClick}
+          className="cursor-pointer p-2 rounded bg-gray-700 hover:bg-gray-600 transition "
+        >
           <Sun />
         </div>
       ) : (
-        <div onClick={handleClick}>
+        <div
+          onClick={handleClick}
+          className="cursor-pointer p-2 bg-gray-100 hover:bg-gray-200 rounded transition"
+        >
           <Moon />
         </div>
       )}
