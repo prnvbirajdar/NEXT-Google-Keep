37c72705eee0cf9a52fa0a9058d3b2876140f2fc
prnvbirajdar <prnvbirajdar@gmail.com>
2021-01-29 12:42:58 -0500

    firebase config

--- assets/firebase/firebase.js
+++ assets/firebase/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSENGER_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCXTLkYc-K7x2wdU81g8c_aK1LezqnRWAU",
+  authDomain: "keep-next-tailwind.firebaseapp.com",
+  databaseURL: "https://keep-next-tailwind-default-rtdb.firebaseio.com",
+  projectId: "keep-next-tailwind",
+  storageBucket: "keep-next-tailwind.appspot.com",
+  messagingSenderId: "318071216631",
+  appId: "1:318071216631:web:e235550f574e898b6eb6f2",
 };
 
 if (firebase.apps.length === 0) {
