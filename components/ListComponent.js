1053ca62beb31b48b7743ab0452b73b58ab78694
prnvbirajdar <prnvbirajdar@gmail.com>
2021-01-31 00:45:47 -0500

    loading

--- components/ListComponent.js
+++ components/ListComponent.js
@@ -1,7 +1,14 @@
 import ListDetail from "./ListDetail";
 
 const ListComponent = ({ mainTodoList }) => {
-  return (
+  console.log(mainTodoList.length);
+  return mainTodoList.length === 0 || undefined || null ? (
+    <div className=" fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden bg-white dark:bg-background opacity-50 flex flex-col items-center justify-center">
+      <h2 className="text-center dark:text-offwhite text-lg lg:text-xl font-medium">
+        Loading...
+      </h2>
+    </div>
+  ) : (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-x-4 gap-y-5 mx-4 sm:mx-12 ">
       {mainTodoList.map((list) => {
         return (
@@ -18,4 +25,3 @@ const ListComponent = ({ mainTodoList }) => {
 };
 
 export default ListComponent;
-
